ab471c37b028b1e00f9fc89193e68d47efaa7170
cm <onurelibol@gmail.com>
2017-09-21 17:58:51 +0300

    Fix for data-* attributes

--- opal/molecule/serializer.rb
+++ opal/molecule/serializer.rb
@@ -1,5 +1,9 @@
 class Element
-  def serialize_hash
-    Hash.new `self.serializeHash()`
+  def serialize_hash(obj)
+    result = Hash.new `self.serializeHash()`
+    result.each do |key, value|
+      obj.instance_variable_set("@#{key}", value)
+    end
+    result
   end
 end
